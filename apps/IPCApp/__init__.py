f31219565e62c5d5726024b661fbb8c02044aab8
BhavaniKallam <36656934+bhavanikallam@users.noreply.github.com>
2022-07-12 21:21:49 +0530

    Update __init__.py

--- apps/IPCApp/__init__.py
+++ apps/IPCApp/__init__.py
@@ -1,3 +1,3 @@
-from IPCApp.TCPIPCServer import IpcTCPRequestHandler
-from IPCApp.expressions import *
+from apps.IPCApp.TCPIPCServer import IpcTCPRequestHandler
+from apps.IPCApp.expressions import *
 
