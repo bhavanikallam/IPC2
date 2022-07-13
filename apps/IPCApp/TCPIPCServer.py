44691fef21df20af698ddd2ff1b847b9d9184952
BhavaniKallam <36656934+bhavanikallam@users.noreply.github.com>
2022-07-13 11:45:30 +0530

    Update TCPIPCServer.py

--- apps/IPCApp/TCPIPCServer.py
+++ apps/IPCApp/TCPIPCServer.py
@@ -3,7 +3,7 @@ import socketserver
 import sys
 import threading
 import traceback
-from apps.IPCApp.expressions import *
+from expressions.functions import evaluate
 
 Error = "error"
 Status = "status"
