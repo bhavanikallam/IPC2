138b7be976c191b2d5cc40f2f4ed77dafbd5ea9f
BhavaniKallam <36656934+bhavanikallam@users.noreply.github.com>
2022-07-13 11:54:03 +0530

    Update TCPIPCServer.py

--- apps/IPCApp/TCPIPCServer.py
+++ apps/IPCApp/TCPIPCServer.py
@@ -67,7 +67,7 @@ class IpcTCPRequestHandler(socketserver.BaseRequestHandler):
 
                 processed_data = None
                 if workType in work_packet and work_packet[workType] == EvaluateExpression:
-                    processed_data = expressions.functions.evaluate(work_packet)
+                    processed_data = evaluate(work_packet)
 
                 elif workType in work_packet and work_packet[workType] == Echo:
                     processed_data = work_packet
